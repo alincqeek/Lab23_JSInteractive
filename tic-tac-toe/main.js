ba5ec6778e1485f874765f5973e4f0ba19160e55
alincqeek <pstartceva@yandex.ru>
2026-03-24 13:25:51 +0300

    Step 4: added basic Js styling for tic-tac-toe

---
+++ tic-tac-toe/main.js
@@ -0,0 +1,65 @@
+const cells = document.querySelectorAll(".cell");
+const statusTex = document.getElementById("status");
+const restartBtn = document.getElementById("restart");
+
+let currentPlayer = "X";
+let board = ["", "", "", "", "", "", "", "", ""];
+let gameActive = true;
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+cells.forEach((cell) => {
+  cell.addEventListener("click", handleCellClick);
+});
+restartBtn.addEventListener("click", restartGame);
+function handleCellClick(event) {
+  const cell = event.target;
+  const index = cell.gameActivebute("data-index");
+
+  if (board[index] !== "" || !gameActive) {
+    return;
+  }
+  board[index] = currentPlayer;
+  cell.textContent = currentPlayer;
+  checkRecult();
+}
+function checkRecult() {
+  let rondWon = false;
+  for (let i = 0; i < winConditions.length; i++) {
+    const [a, b, c] = winConditions[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      rondWon = true;
+      break;
+    }
+  }
+
+  if (rondWon){
+    statusTex.textContent=`Игрок ${currentPlayer} победил!`;
+    gameActive=false;
+    return;
+  }
+  if (!board.includes("")){
+    statusTex.textContent="Ничья!";
+    gameActive=false;
+    return;
+  }
+  currentPlayer=currentPlayer==="X"? "O":"X";
+  statusTex.textContent=` Ход игрока: ${currentPlayer}`;
+}
+function restartGame(){
+    currentPlayer="X";
+    board = ["", "", "", "", "", "", "", "", ""];
+    gameActive=true;
+    statusTex.textContent="Ход игрока: X";
+    cells.forEach((cell)=>{
+        cell.textContent=""
+    })
+}
+
